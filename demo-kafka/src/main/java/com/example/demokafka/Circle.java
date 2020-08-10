200d4713ee5b76ff74ae64cfaa739f9ded0d0678
haotingfang <haotingfang@souche.com>
2020-08-10 23:13:15 +0800

    circle并没有冲突  为啥会变绿呢

--- demo-kafka/src/main/java/com/example/demokafka/Circle.java
+++ demo-kafka/src/main/java/com/example/demokafka/Circle.java
@@ -11,4 +11,8 @@ public class Circle extends Shape {
     public void show(){
         System.out.println("----这是一个Circle");
     }
+
+    public void show1(){
+        System.out.println("----这是一个Circle");
+    }
 }
